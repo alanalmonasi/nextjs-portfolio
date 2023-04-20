26c9c306bfa9f9097aa705cf3a2201984c169771
alanalmonasi <almonasialan@gmail.com>
2023-04-20 16:03:42 -0600

    updated heroSection

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -19,7 +19,7 @@ export const HeroSection = () => {
             </div>
             <div className="md:mt-2 md:w-3/5">
                <h1 className="font-bold text-4xl mt-6 md:text-7xl md:mt-0">
-                  Hi I&#39;m Alan!
+                  Hi I&#39;m Alan
                </h1>
                <p className="text-lg mt-4 mb-6 md:text-2xl">
                   I&#39;m a{' '}
